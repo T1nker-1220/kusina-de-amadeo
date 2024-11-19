38ee95eb9934e105f010298ba70b093860b7d68f
T1nker-1220 <marquezjohnnathanieljade@gmail.com>
2024-11-19 20:58:51 +0800

    fix: update next.config.js for Vercel deployment

--- next.config.js
+++ next.config.js
@@ -20,6 +20,12 @@ const nextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        port: '',
+        pathname: '/**',
+      }
     ],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
@@ -28,7 +34,7 @@ const nextConfig = {
   },
   experimental: {
     serverActions: {
-      allowedOrigins: ['localhost:3000'],
+      allowedOrigins: ['localhost:3000', 'kusina-de-amadeo.vercel.app'],
       bodySizeLimit: '2mb'
     },
   },
